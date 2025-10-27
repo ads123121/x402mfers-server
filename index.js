92d59aeee4380a28d628c5043a3303634e622773
ads123121 <149018818+ads123121@users.noreply.github.com>
2025-10-27 21:06:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const app = express();
 
 app.use(express.json());
 
-// Главный x402 endpoint
 app.post("/mint", (req, res) => {
   res.status(402).json({
-    x402Version: "1.0.0",
+    x402Version: 1.0,
     error: "payment_required",
     accepts: [
       {
         scheme: "exact",
         network: "base",
         maxAmountRequired: "1000000",
-        resource: "nft_mint",
+        resource: "https://x402mfers-server.vercel.app/mint",
         description: "Mint NFT on Base",
-        simeType: "USDC",
+        mimeType: "application/json",
         payTo: "0xf486085502ED56Bf83a833c12cF44f521B5806E9",
         maxTimeoutSeconds: 300,
         asset: "USDC"
@@ -24,7 +23,6 @@ app.post("/mint", (req, res) => {
   });
 });
 
-// Тестовый endpoint
 app.get("/", (req, res) => {
   res.json({
     status: "ok",
